31b1e676387ac0e7346a0f5c010e2c7475bec67d
taikulawo <qaq1362211689@gmail.com>
2024-08-18 00:52:27 +0800

    1

--- examples/hello_runtime.js
+++ examples/hello_runtime.js
@@ -1,3 +1,4 @@
 globalThis.rustCallback = function rustCallback(data) {
     console.log("Receive rust message:", data);
+    return 1
 };
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@
 
 use deno_runtime::deno_core::error::AnyError;
 use deno_runtime::deno_core::resolve_path;
+use deno_runtime::deno_core::serde_json;
+use deno_runtime::deno_core::serde_json::Value;
+use deno_runtime::deno_core::serde_v8;
 use deno_runtime::deno_core::ModuleCodeString;
+use deno_runtime::deno_napi::v8;
 use deno_runtime::deno_permissions::Permissions;
 use deno_runtime::deno_permissions::PermissionsContainer;
 use deno_runtime::worker::MainWorker;
@@ -21,8 +25,26 @@ async fn main() -> Result<(), AnyError> {
     worker.execute_main_module(&main_module).await?;
     let code = "rustCallback('lol')";
     let result = worker.execute_script("", ModuleCodeString::from_static(code));
-    if let Err(err) = result {
-        println!("execute_mod err {:?}", err);
+    match result {
+        Err(err) => {
+            println!("execute_mod err {:?}", err);
+        }
+        Ok(res) => {
+            let mut scope = worker.js_runtime.handle_scope();
+            let local = v8::Local::new(&mut scope, res);
+            let deserialized_value = serde_v8::from_v8::<serde_json::Value>(&mut scope, local);
+            match deserialized_value {
+                Ok(x) => match x {
+                    Value::Number(b) => {
+                        println!("res {b}")
+                    }
+                    _ => panic!("unknown data return type"),
+                },
+                Err(err) => {
+                    eprintln!("{err}");
+                }
+            }
+        }
     }
     println!("execute_main_module end");
     worker.run_event_loop(false).await?;
